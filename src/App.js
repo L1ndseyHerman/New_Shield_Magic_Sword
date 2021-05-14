82915d4dbef23deb599a2d2ef29316a9ee268e2d
L1ndseyHerman <lherman21@jcu.edu>
2021-05-14 00:14:27 -0400

    Redid app in a new folder with npx create-react-app my-app --template cra-template-pwa-typescript, because why code a service worker myself when I could just use an auto-generated one?

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function makeComputerChoice()
+{
+  const computerNumber = Math.floor(Math.random() * 3); 
+
+  if (computerNumber === 0)
+  {
+    return "Rock";
+  }
+  else if (computerNumber === 1)
+  {
+    return "Paper";
+  }
+  else 
+  {
+    return "Scissors";
+  }
+}
+
+function checkWinLooseTie(userChoice, computerChoice)
+{
+  if (userChoice === "Rock" && computerChoice === "Rock")
+  {
+    return "Tie.";
+  }
+  else if (userChoice === "Rock" && computerChoice === "Paper")
+  {
+    return "Computer wins!";
+  }
+  else if (userChoice === "Rock" && computerChoice === "Scissors")
+  {
+    return "Player1 wins!";
+  }
+
+  else if (userChoice === "Paper" && computerChoice === "Rock")
+  {
+    return "Player1 wins!";
+  }
+  else if (userChoice === "Paper" && computerChoice === "Paper")
+  {
+    return "Tie.";
+  }
+  else if (userChoice === "Paper" && computerChoice === "Scissors")
+  {
+    return "Computer wins!";
+  }
+
+  else if (userChoice === "Scissors" && computerChoice === "Rock")
+  {
+    return "Computer wins!";
+  }
+  else if (userChoice === "Scissors" && computerChoice === "Paper")
+  {
+    return "Player1 wins!";
+  }
+  else if (userChoice === "Scissors" && computerChoice === "Scissors")
+  {
+    return "Tie.";
+  }
+
+  return "This is an error. Click a button to start a game.";
+}
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  /*  
+    This only updates things if the state changes, bec useState.
+    For example, if you keep pressing the Rock button,
+    it will check "Is Rock === Rock?", and since it is, no computer number is generated, the DOM
+    doesn't update, etc. But if you press Paper, "Is Rock===Paper?" is false, and so the stuff updates.
+
+    To make Rock update every button press, do useEffect.
+  */
+    const [userChoice, setUserChoice] = useState("An Error");
+    var paragraphText = "";
+  
+    if (userChoice !== "An Error")
+    {
+      const computerChoice = makeComputerChoice();
+      const winLooseTieText = checkWinLooseTie(userChoice, computerChoice);
+      paragraphText = "Player1 chose " + userChoice +
+       " and Computer chose " + computerChoice + ". " + winLooseTieText;
+    }
+  
+    return (
+      <main>  
+        <h1>Rock-Paper-Scissors</h1>
+        <button onClick={() => setUserChoice("Rock")}>Rock</button>
+        <button onClick={() => setUserChoice("Paper")}>Paper</button>
+        <button onClick={() => setUserChoice("Scissors")}>Scissors</button>
+        <p>{paragraphText}</p>
+        <p id="thePar">Try to beat the computer at Rock-Paper-Scissors. You cannot make the same choice more than
+        once in a row.</p>
+        <p>And check out <a id="aLink1" href="https://l1ndseyherman.github.io/">My Other Website</a>.</p>
+        <footer id="theFooter">Not a PWA yet, updated 05/14/21</footer>
+      </main>
+    );
 }
 
 export default App;
