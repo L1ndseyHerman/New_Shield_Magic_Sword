199ba8a0baf3e4dde19b482c44bb52f321b1cde4
L1ndseyHerman <lherman21@jcu.edu>
2021-05-17 22:27:13 -0400

    Updated date, deleted useEffect() code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import React, { useState, useEffect } from 'react';
 import React, { useState } from 'react';
 import './App.css';
 
@@ -64,62 +63,8 @@ function checkWinLooseTie(userChoice, computerChoice)
   return "This is an error. Click a button to start a game.";
 }
 
-/*function disableButton(userChoice)
-{
-  if (userChoice === "Rock")
-  {
-
-  }
-}*/
-
 function App() {
-  /*  
-    This only updates things if the state changes, bec useState.
-    For example, if you keep pressing the Rock button,
-    it will check "Is Rock === Rock?", and since it is, no computer number is generated, the DOM
-    doesn't update, etc. But if you press Paper, "Is Rock===Paper?" is false, and so the stuff updates.
-
-    To make Rock update every button press, do useEffect.
-  */
-
-    /*const [userChoice, setUserChoice] = useState("An Error");
-    var paragraphText = "";
-
-    //useEffect(() => {
-      //var userChoice = "An Error";
-      //function setUserChoice(userChoice) {
-        //var paragraphText = "";
-  
-        //if (userChoice !== "An Error")
-        //{
-          const computerChoice = makeComputerChoice();
-          const winLooseTieText = checkWinLooseTie(userChoice, computerChoice);
-          paragraphText = "Player1 chose " + userChoice +
-          " and Computer chose " + computerChoice + ". " + winLooseTieText;
-        //}
-      //}
-    });
-    */
-
-
-    
-    /*useEffect(() => {
-      function handleStatusChange(status) {
-        setIsOnline(status.isOnline);
-      }*/
-    
-    //const [userChoice, setUserChoice] = useEffect("An Error");
-    /*useEffect(() => {
-      // Update the document title using the browser API
-      document.title = `You clicked ${count} times`;
-    });*/
-
-
-
-
-
     const [userChoice, setUserChoice] = useState("An Error");
-    //var disabledButtons = [false, false, false];
     const [disabledButtons, setDisabledButton] = useState([false, false, false]);
     var paragraphText = "";
   
@@ -129,9 +74,6 @@ function App() {
       const winLooseTieText = checkWinLooseTie(userChoice, computerChoice);
       paragraphText = "Player1 chose " + userChoice +
        " and Computer chose " + computerChoice + ". " + winLooseTieText;
-
-       // New! Disabling the button that was just pressed:
-
     }
   
     return (
@@ -147,7 +89,7 @@ function App() {
         <p id="thePar">Try to beat the computer at Rock-Paper-Scissors. You cannot make the same choice more than
         once in a row.</p>
         <p>And check out <a id="aLink1" href="https://l1ndseyherman.github.io/">My Other Website</a>.</p>
-        <footer id="theFooter">This app is a PWA now! Updated 05/14/21</footer>
+        <footer id="theFooter">This app is a PWA now! Updated 05/17/21</footer>
       </main>
     );
 }
