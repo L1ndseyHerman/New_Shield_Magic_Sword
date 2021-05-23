0567a98d19810b3c48d9ddc3d8675038bee683a0
L1ndseyHerman <lherman21@jcu.edu>
2021-05-22 23:32:40 -0400

    Testing persistant storage for PlayerOneHealth + Shield

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function makeComputerChoice()
@@ -7,55 +7,55 @@ function makeComputerChoice()
 
   if (computerNumber === 0)
   {
-    return "Rock";
+    return "Shield";
   }
   else if (computerNumber === 1)
   {
-    return "Paper";
+    return "Magic";
   }
   else 
   {
-    return "Scissors";
+    return "Sword";
   }
 }
 
 function checkWinLooseTie(userChoice, computerChoice)
 {
-  if (userChoice === "Rock" && computerChoice === "Rock")
+  if (userChoice === "Shield" && computerChoice === "Shield")
   {
     return "Tie.";
   }
-  else if (userChoice === "Rock" && computerChoice === "Paper")
+  else if (userChoice === "Shield" && computerChoice === "Magic")
   {
     return "Computer wins!";
   }
-  else if (userChoice === "Rock" && computerChoice === "Scissors")
+  else if (userChoice === "Shield" && computerChoice === "Sword")
   {
     return "Player1 wins!";
   }
 
-  else if (userChoice === "Paper" && computerChoice === "Rock")
+  else if (userChoice === "Magic" && computerChoice === "Shield")
   {
     return "Player1 wins!";
   }
-  else if (userChoice === "Paper" && computerChoice === "Paper")
+  else if (userChoice === "Magic" && computerChoice === "Magic")
   {
     return "Tie.";
   }
-  else if (userChoice === "Paper" && computerChoice === "Scissors")
+  else if (userChoice === "Magic" && computerChoice === "Sword")
   {
     return "Computer wins!";
   }
 
-  else if (userChoice === "Scissors" && computerChoice === "Rock")
+  else if (userChoice === "Sword" && computerChoice === "Shield")
   {
     return "Computer wins!";
   }
-  else if (userChoice === "Scissors" && computerChoice === "Paper")
+  else if (userChoice === "Sword" && computerChoice === "Magic")
   {
     return "Player1 wins!";
   }
-  else if (userChoice === "Scissors" && computerChoice === "Scissors")
+  else if (userChoice === "Sword" && computerChoice === "Sword")
   {
     return "Tie.";
   }
@@ -68,6 +68,18 @@ function App()
   const [userChoice, setUserChoice] = useState("An Error");
   const [disabledButtons, setDisabledButton] = useState([false, false, false]);
   var paragraphText = "";
+  var [playerOneHealth, setPlayerOneHealth] = useState(10);
+  var [computerHealth, setComputerHealth] = useState(10);
+
+  //  Gets it from local, temp browser storage, or something.
+  React.useEffect(() => {
+    const playerOneStoredHealth = Number(localStorage.getItem("playerOneHealth") || 10)
+    setPlayerOneHealth(playerOneStoredHealth)
+  }, [])
+
+  React.useEffect(() => {
+    localStorage.setItem("playerOneHealth", playerOneHealth)
+  }, [playerOneHealth])
 
   if (userChoice !== "An Error")
   {
@@ -82,19 +94,19 @@ function App()
       <div id="whereIsTheOtherDiv">
         <h1>Shield-Magic-Sword</h1>
         <div id="buttonsAndDescriptionsDiv">
-          <button id="shield" disabled={disabledButtons[0]} onClick={() => {setUserChoice("Rock");
-            setDisabledButton([true, false, false]);}}>Shield</button>
+          <button id="shield" disabled={disabledButtons[0]} onClick={() => {setUserChoice("Shield");
+            setDisabledButton([true, false, false]); setPlayerOneHealth(p1h => p1h - 1)}}>Shield</button>
           <label>Blocks two physical damage.</label>
-          <button id="magic" disabled={disabledButtons[1]} onClick={() => {setUserChoice("Paper");
+          <button id="magic" disabled={disabledButtons[1]} onClick={() => {setUserChoice("Magic");
             setDisabledButton([false, true, false]);}}>Magic</button>
           <label>Deals one magic damage.</label>
-          <button id="sword" disabled={disabledButtons[2]} onClick={() => {setUserChoice("Scissors");
+          <button id="sword" disabled={disabledButtons[2]} onClick={() => {setUserChoice("Sword");
             setDisabledButton([false, false, true]);}}>Sword</button>
           <label>Deals two physical damage.</label>
         </div>
         <div id="healthAndTurnInfoDiv">
-          <p id="playerOneHealth">Player1 health: 10</p>
-          <p id="computerHealth">Computer health: 10</p>
+          <p id="playerOneHealth">Player1 health: {playerOneHealth}</p>
+          <p id="computerHealth">Computer health: {computerHealth}</p>
         </div>
         <br/>
         <br/>
@@ -103,7 +115,7 @@ function App()
             One <a href="https://github.com/L1ndseyHerman/New_Shield_Magic_Sword/tree/Version-1-Branch">
             Here</a>.</p>
           <p>And check out <a href="https://l1ndseyherman.github.io/">My Other Website</a>.</p>
-          <footer id="theFooter">This is a React JS Progressive Web App. Updated 05/20/21.</footer>
+          <footer id="theFooter">This is a React JS Progressive Web App. Updated 05/22/21.</footer>
       </div>
     </main>
   );
