a996ac0e26f93b8e0840e21f67fa39c05c2cf9b7
L1ndseyHerman <lherman21@jcu.edu>
2021-06-14 22:58:13 -0400

    Switched back to master branch

--- src/App.js
+++ src/App.js
@@ -240,13 +240,16 @@ function App()
           <button onClick={() => {setIsNotNewGame(false);
             setUserChoice("First Turn"); setDisabledButton([false, false, false]);}}>New Game</button>
         </div>
-        <p id="thePar">Done with Version 2.1, which is global CSS that has responsive computer 
-          vs phone versions based on screen width. The app also has a logo I drew. It does
-          not have gameplay changes. If you are a software developer who wants to look at 
+        <p id="thePar">
+          Starting Version 2.2, which will be fixing mistakes. After watching TechWithTim's 
+          first eight ReactJS videos on YouTube, the first of which
+          is <a href="https://www.youtube.com/watch?v=Ma6DRDIedVE&t=1s">Here</a> , I realized
+          I am not creating multiple React components the way I should be, so I will be
+          fixing that. If you are a software developer who wants to look at 
           previous versions, you can go to
           my <a href="https://github.com/L1ndseyHerman/New_Shield_Magic_Sword/">
           GitHub</a>, change the branch from &quot;master&quot; to the version you are looking for,
-          and run the code in Visual Studio Code.</p>
+          and run the code in VS Code.</p>
         <p>And check out <a href="https://l1ndseyherman.github.io/">My Other Website</a>.</p>
         <footer>This is a React JS Progressive Web App. Updated 06/14/21.</footer>
       </div>
