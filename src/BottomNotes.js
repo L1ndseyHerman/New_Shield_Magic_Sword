50b10370bb56c777922a7d7aa0e07dfb345cec6f
L1ndseyHerman <lherman21@jcu.edu>
2021-06-18 19:25:59 -0400

    Version 2.2 mainly uses Components, props and callbacks, unlike previous versions. There is also Component-specific CSS and button divs that appear/dissappear.

--- src/BottomNotes.js
+++ src/BottomNotes.js
@@ -8,15 +8,18 @@ function BottomNotes()
     return(
         <div>
             <p>
-                Starting Version 2.2, which will be fixing mistakes. After watching TechWithTim's 
-                first eight ReactJS videos on YouTube, the first of which
-                is <a href="https://www.youtube.com/watch?v=Ma6DRDIedVE&t=1s">Here</a>, I realized
-                I am not creating multiple React components the way I should be, so I will be
-                fixing that. If you are a software developer who wants to look at 
+                Version 2.2 is done! If you are a software developer who wants to look at 
                 previous versions, you can go to
                 my <a href="https://github.com/L1ndseyHerman/New_Shield_Magic_Sword/">
-                GitHub</a>, change the branch from &quot;master&quot; to the version you are looking for,
-                and run the code in VS Code.</p>
+                GitHub</a>, change the branch from &quot;master&quot; to the version 
+                you are looking for, then pull the code and run it in VS Code.
+            </p><p>    
+                I'd also like to shout out Tech With Tim's 
+                first ten ReactJS videos on YouTube, the first of which
+                is <a href="https://www.youtube.com/watch?v=Ma6DRDIedVE&t=1s">Here</a>,  
+                for being a great ReactJS learning resource. Tim's series is in-progress,
+                so there will eventually be more than ten videos.
+            </p>
             <p>And check out <a href="https://l1ndseyherman.github.io/">My Other Website</a>.</p>
             <footer>This is a React JS Progressive Web App. Updated 06/18/21</footer>
         </div>
