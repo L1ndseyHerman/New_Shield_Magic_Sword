4a68ac013a51209a926d13be4937d4cf865de20d
L1ndseyHerman <lherman21@jcu.edu>
2021-07-15 16:37:23 -0400

    GameButtonWithExplanation.test.js done

--- src/GameButtonWithExplanation.test.js
+++ src/GameButtonWithExplanation.test.js
@@ -18,14 +18,86 @@ afterEach(() => {
   container = null;
 });
 
-it("renders explanation text", () => {
+it("renders enabled Shield button + explanation", () => {
   
   act(() => {
-    render(<GameButtonWithExplanation buttonText="Shield" 
-      explanation="Blocks two physical damage." />, container);
+    render(<GameButtonWithExplanation buttonColor="darkolivegreen" buttonText="Shield" 
+      explanation="Blocks two physical damage." isDisabled={false} />, container);
   });
 
+  expect(container.querySelector("button").style.background).toBe("darkolivegreen");
+  expect(container.querySelector("button").disabled).toBe(false);
   expect(container.querySelector("button").textContent).toBe("Shield");
   expect(container.querySelector("p").textContent).toBe("Blocks two physical damage.");
+
+});
+
+it("renders disabled Shield button + explanation", () => {
+  
+  act(() => {
+    render(<GameButtonWithExplanation buttonColor="darkolivegreen" buttonText="Shield" 
+      explanation="Blocks two physical damage." isDisabled={true} />, container);
+  });
+
+  expect(container.querySelector("button").style.background).toBe("darkolivegreen");
+  expect(container.querySelector("button").disabled).toBe(true);
+  expect(container.querySelector("button").textContent).toBe("Shield");
+  expect(container.querySelector("p").textContent).toBe("Blocks two physical damage.");
+
+});
+
+it("renders enabled Magic button + explanation", () => {
+  
+  act(() => {
+    render(<GameButtonWithExplanation buttonColor="royalblue" buttonText="Magic" 
+      explanation="Deals one magic damage." isDisabled={false} />, container);
+  });
+
+  expect(container.querySelector("button").style.background).toBe("royalblue");
+  expect(container.querySelector("button").disabled).toBe(false);
+  expect(container.querySelector("button").textContent).toBe("Magic");
+  expect(container.querySelector("p").textContent).toBe("Deals one magic damage.");
+
+});
+
+it("renders disabled Magic button + explanation", () => {
+  
+  act(() => {
+    render(<GameButtonWithExplanation buttonColor="royalblue" buttonText="Magic" 
+      explanation="Deals one magic damage." isDisabled={true} />, container);
+  });
+
+  expect(container.querySelector("button").style.background).toBe("royalblue");
+  expect(container.querySelector("button").disabled).toBe(true);
+  expect(container.querySelector("button").textContent).toBe("Magic");
+  expect(container.querySelector("p").textContent).toBe("Deals one magic damage.");
+
+});
+
+it("renders enabled Sword button + explanation", () => {
+  
+  act(() => {
+    render(<GameButtonWithExplanation buttonColor="firebrick" buttonText="Sword" 
+      explanation="Deals two physical damage." isDisabled={false} />, container);
+  });
+
+  expect(container.querySelector("button").style.background).toBe("firebrick");
+  expect(container.querySelector("button").disabled).toBe(false);
+  expect(container.querySelector("button").textContent).toBe("Sword");
+  expect(container.querySelector("p").textContent).toBe("Deals two physical damage.");
+
+});
+
+it("renders disabled Sword button + explanation", () => {
+  
+  act(() => {
+    render(<GameButtonWithExplanation buttonColor="firebrick" buttonText="Sword" 
+      explanation="Deals two physical damage." isDisabled={true} />, container);
+  });
+
+  expect(container.querySelector("button").style.background).toBe("firebrick");
+  expect(container.querySelector("button").disabled).toBe(true);
+  expect(container.querySelector("button").textContent).toBe("Sword");
+  expect(container.querySelector("p").textContent).toBe("Deals two physical damage.");
 
 });
