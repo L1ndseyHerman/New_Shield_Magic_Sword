6d5656b79c3cb148eac9338cd3690ad6c1f758e0
L1ndseyHerman <lherman21@jcu.edu>
2021-07-15 01:20:12 -0400

    Wrote a second test in the same .test.js

--- src/ChooseElementButtonWithExplanation.test.js
+++ src/ChooseElementButtonWithExplanation.test.js
@@ -18,10 +18,8 @@ afterEach(() => {
   container = null;
 });
 
-it("renders explanation text", () => {
+it("renders Fire button + explanation", () => {
 
-  //  For all of these tests, just testing the first (left-most) component, since the 
-  //  other five should do sim things.
   act(() => {
     render(<ChooseElementButtonWithExplanation buttonText="Fire" explanation="Earth"
       buttonColor="firebrick" explanationColor="darkolivegreen" />, container);
@@ -30,7 +28,20 @@ it("renders explanation text", () => {
   expect(container.querySelector("button").textContent).toBe("Fire");
   expect(container.querySelector("button").style.background).toBe("firebrick");
   expect(container.querySelector("p").textContent).toBe("+1 damage to Earth.");
-  //  GOT COLORS WORKING THIS WAY!!! :D :D :D
   expect(container.querySelector("span").style.color).toBe("darkolivegreen");
+
+});
+
+it("renders Earth button + explanation", () => {
+
+  act(() => {
+    render(<ChooseElementButtonWithExplanation buttonText="Earth" explanation="Air"
+      buttonColor="darkolivegreen" explanationColor="darkorange" />, container);
+  });
+  
+  expect(container.querySelector("button").textContent).toBe("Earth");
+  expect(container.querySelector("button").style.background).toBe("darkolivegreen");
+  expect(container.querySelector("p").textContent).toBe("+1 damage to Air.");
+  expect(container.querySelector("span").style.color).toBe("darkorange");
 
 });
